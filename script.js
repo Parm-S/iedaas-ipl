8d0e451499ef5c4e47efc820aecad1bf7c4acbd9
Parminder Singh <89296932+Parm-S@users.noreply.github.com>
2021-09-02 14:43:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,82 @@
+const cskTeamName = document.querySelector('#card-csk-teamName');
+const cskVenue = document.querySelector('#card-csk-venue');
+const cskWinningYears = document.querySelector('#card-csk-winningYear');
+
+const dcTeamName = document.querySelector('#card-dc-teamName');
+const dcVenue = document.querySelector('#card-dc-venue');
+
+const pkTeamName = document.querySelector('#card-pk-teamName');
+const pkVenue = document.querySelector('#card-pk-venue');
+
+const kkrTeamName = document.querySelector('#card-kkr-teamName');
+const kkrVenue = document.querySelector('#card-kkr-venue');
+const kkrWinningYears = document.querySelector('#card-kkr-winningYear');
+
+const miTeamName = document.querySelector('#card-mi-teamName');
+const miVenue = document.querySelector('#card-mi-venue');
+const miWinningYears = document.querySelector('#card-mi-winningYear');
+
+const rrTeamName = document.querySelector('#card-rr-teamName');
+const rrVenue = document.querySelector('#card-rr-venue');
+const rrWinningYears = document.querySelector('#card-rr-winningYear');
+
+const rcbTeamName = document.querySelector('#card-rcb-teamName');
+const rcbVenue = document.querySelector('#card-rcb-venue');
 
 
-    fetch('https://ipl-t20.herokuapp.com/teams',{
-       
-        headers:{
-            Accept :'application/json',
-            'Access-Control-Allow-Origin': '*'
+const srhTeamName = document.querySelector('#card-srh-teamName');
+const srhVenue = document.querySelector('#card-srh-venue');
+const srhWinningYears = document.querySelector('#card-srh-winningYear');
 
-        }
+
+
+const setHeaders = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+    mode: 'cors',
+    cache: 'default'
+
+};
+
+fetch('https://cors-anywhere.herokuapp.com/https://ipl-t20.herokuapp.com/teams', setHeaders)
+    .then((data) => {
+        return data.json();
+    }).then((resData) => {
+        console.log(resData[0].winningYears)
+        cskTeamName.innerHTML = resData[0].teamName;
+        cskVenue.innerHTML = resData[0].venue;
+        cskWinningYears.innerHTML = resData[0].winningYears;
+
+        dcTeamName.innerHTML = resData[1].teamName;
+        dcVenue.innerHTML = resData[1].venue;
+
+        pkTeamName.innerHTML = resData[2].teamName;
+        pkVenue.innerHTML = resData[2].venue;
+
+        kkrTeamName.innerHTML = resData[3].teamName;
+        kkrVenue.innerHTML = resData[3].venue;
+        kkrWinningYears.innerHTML = resData[3].winningYears;
+
+        miTeamName.innerHTML = resData[4].teamName;
+        miVenue.innerHTML = resData[4].venue;
+        miWinningYears.innerHTML = resData[4].winningYears;
+
+        rrTeamName.innerHTML = resData[5].teamName;
+        rrVenue.innerHTML = resData[5].venue;
+        rrWinningYears.innerHTML = resData[5].winningYears;
+
+        rcbTeamName.innerHTML = resData[6].teamName;
+        rcbVenue.innerHTML = resData[6].venue;
+
+
+        srhTeamName.innerHTML = resData[7].teamName;
+        srhVenue.innerHTML = resData[7].venue;
+        srhWinningYears.innerHTML = resData[7].winningYears;
     })
-    .then((response)=>{
-         return response.json();
-    }).then((data)=>{
-        const[csk,dc,pk,kkr,mi,rr,rcb,srh]=data;
-        console.log(csk);
-        
-    }).catch((error)=>{console.log(error);});
+    .catch((error) => {
+        console.log(error);
+    })
+
