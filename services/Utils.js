bcb2bd0ae51ad9411b2e81d397d569e65a19b6c5
Parminder Singh <89296932+Parm-S@users.noreply.github.com>
2021-09-05 22:33:30 +0530

    Add files via upload

---
+++ services/Utils.js
@@ -0,0 +1,24 @@
+const Utils = { 
+    // --------------------------------
+    //  Parse a url and break it into resource, id and verb
+    // --------------------------------
+    parseRequestURL : () => {
+
+        let url = location.hash.slice(2).toLowerCase() || '/';
+        let r = url.split("/")
+        let request = {
+            resource    : null
+        }
+        request.resource    = r[1]
+        return request
+    }
+
+    // --------------------------------
+    //  Simple sleep implementation
+    // --------------------------------
+    , sleep: (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+}
+
+export default Utils;
