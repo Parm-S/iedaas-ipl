a55f00d7e7ecc605924626cf4c394dd0f2626191
Parminder Singh <89296932+Parm-S@users.noreply.github.com>
2021-09-05 22:39:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,203 +1,46 @@
-// const cskTeamName = document.querySelector('#card-csk-teamName');
-// const cskVenue = document.querySelector('#card-csk-venue');
-// const cskWinningYears = document.querySelector('#card-csk-winningYear');
-
-// const dcTeamName = document.querySelector('#card-dc-teamName');
-// const dcVenue = document.querySelector('#card-dc-venue');
-
-// const pkTeamName = document.querySelector('#card-pk-teamName');
-// const pkVenue = document.querySelector('#card-pk-venue');
-
-// const kkrTeamName = document.querySelector('#card-kkr-teamName');
-// const kkrVenue = document.querySelector('#card-kkr-venue');
-// const kkrWinningYears = document.querySelector('#card-kkr-winningYear');
-
-// const miTeamName = document.querySelector('#card-mi-teamName');
-// const miVenue = document.querySelector('#card-mi-venue');
-// const miWinningYears = document.querySelector('#card-mi-winningYear');
-
-// const rrTeamName = document.querySelector('#card-rr-teamName');
-// const rrVenue = document.querySelector('#card-rr-venue');
-// const rrWinningYears = document.querySelector('#card-rr-winningYear');
-
-// const rcbTeamName = document.querySelector('#card-rcb-teamName');
-// const rcbVenue = document.querySelector('#card-rcb-venue');
-
-
-// const srhTeamName = document.querySelector('#card-srh-teamName');
-// const srhVenue = document.querySelector('#card-srh-venue');
-// const srhWinningYears = document.querySelector('#card-srh-winningYear');
-
-const teamsNames = [
-    document.querySelector('#card-csk-teamName'),
-    document.querySelector('#card-dc-teamName'),
-    document.querySelector('#card-pk-teamName'),
-    document.querySelector('#card-kkr-teamName'),
-    document.querySelector('#card-mi-teamName'),
-    document.querySelector('#card-rr-teamName'),
-    document.querySelector('#card-rcb-teamName'),
-    document.querySelector('#card-srh-teamName'),
-];
-const teamsVenue = [
-    document.querySelector('#card-csk-venue'),
-    document.querySelector('#card-dc-venue'),
-    document.querySelector('#card-pk-venue'),
-    document.querySelector('#card-kkr-venue'),
-    document.querySelector('#card-mi-venue'),
-    document.querySelector('#card-rr-venue'),
-    document.querySelector('#card-rcb-venue'),
-    document.querySelector('#card-srh-venue')
-]
-const teamsWin = [
-    document.querySelector('#card-csk-winningYear'),
-    document.querySelector('#card-dc-winningYear'),
-    document.querySelector('#card-pk-winningYear'),
-    document.querySelector('#card-kkr-winningYear'),
-    document.querySelector('#card-mi-winningYear'),
-    document.querySelector('#card-rr-winningYear'),
-    document.querySelector('#card-rcb-winningYear'),
-    document.querySelector('#card-srh-winningYear')
-]
-let teamData = [
-
-    // {
-    //     "id": "chennai-super-kings",
-    //     "teamName": "Chennai Super Kings",
-    //     "winningYears": [
-    //         2010,
-    //         2011,
-    //         2018
-    //     ],
-    //     "venue": "M. A. Chidambaram Stadium"
-    // },
-    // {
-    //     "id": "delhi-capitals",
-    //     "teamName": "Delhi Capitals",
-    //     "winningYears": [],
-    //     "venue": "Feroz Shah Kotla Ground"
-    // },
-    // {
-    //     "id": "kings-xi-punjab",
-    //     "teamName": "Kings XI Punjab",
-    //     "winningYears": [],
-    //     "venue": "IS Bindra Stadium"
-    // },
-    // {
-    //     "id": "kolkata-knight-riders",
-    //     "teamName": "Kolkata Knight Riders",
-    //     "winningYears": [
-    //         2012,
-    //         2014
-    //     ],
-    //     "venue": "Eden Gardens"
-    // },
-    // {
-    //     "id": "mumbai-indians",
-    //     "teamName": "Mumbai Indians",
-    //     "winningYears": [
-    //         2013,
-    //         2015,
-    //         2017,
-    //         2019
-    //     ],
-    //     "venue": "Wankhede Stadium"
-    // },
-    // {
-    //     "id": "rajasthan-royals",
-    //     "teamName": "Rajasthan Royals",
-    //     "winningYears": [
-    //         2008
-    //     ],
-    //     "venue": "Sawai Mansingh Stadium"
-    // },
-    // {
-    //     "id": "royal-challengers-bangalore",
-    //     "teamName": "Royal Challengers Bangalore",
-    //     "winningYears": [],
-    //     "venue": "M. Chinnaswamy Stadium"
-    // },
-    // {
-    //     "id": "sunrisers-hyderabad",
-    //     "teamName": "Sunrisers Hyderabad",
-    //     "winningYears": [
-    //         2016
-    //     ],
-    //     "venue": "Rajiv Gandhi Intl. Cricket Stadium"
-    // }
-];
-
-
-
-const setHeaders = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-    mode: 'cors',
-    cache: 'default'
-
+import Dc from './views/pages/Dc.js';
+import Csk from './views/pages/Csk.js';
+import Pk from './views/pages/Pk.js';
+import Kkr from './views/pages/Kkr.js';
+import Mi from './views/pages/Mi.js';
+import Rcb from './views/pages/Rcb.js';
+import Rr from './views/pages/Rr.js';
+import Srh from './views/pages/Srh.js';
+import Error404 from './views/pages/Error.js';
+import Utils from './services/Utils.js';
+import Teams from './views/pages/teams.js';
+// List of supported routes. Any url other than these routes will throw a 404 error
+const routes = {
+    '/': Teams,
+    '/teams': Teams,
+    '/chennai-super-kings': Csk,
+    '/delhi-capital': Dc,
+    '/punjab-kings': Pk,
+    '/kolkata-knight-rider': Kkr,
+    '/mumbai-indians': Mi,
+    '/rajasthan-royal': Rr,
+    '/royal-challenger-banglore': Rcb,
+    '/sunriser-hyderabad': Srh
 };
 
-fetch('https://cors-anywhere.herokuapp.com/https://ipl-t20.herokuapp.com/teams', setHeaders)
-    .then((data) => {
-        return data.json();
-    }).then((resData) => {
-        // console.log(resData[0].winningYears)
-        teamData = resData;
-        console.log(teamData);
-        for (let i = 0; i < teamData.length; i++) {
-            console.log(teamsNames[i]);
-            console.log(teamData[i]);
-            teamsNames[i].innerHTML = teamData[i].teamName;
-            teamsVenue[i].innerHTML = teamData[i].venue;
-            if (teamData[i].winningYears.length != 0) {
-                teamsWin[i].innerHTML = teamData[i].winningYears;
-            } else {
-                teamsWin[i].parentElement.style.visibility = "hidden";
-            }
-        }
-        // cskTeamName.innerHTML = resData[0].teamName;
-        // cskVenue.innerHTML = resData[0].venue;
-        // cskWinningYears.innerHTML = resData[0].winningYears;
 
-        // dcTeamName.innerHTML = resData[1].teamName;
-        // dcVenue.innerHTML = resData[1].venue;
-
-        // pkTeamName.innerHTML = resData[2].teamName;
-        // pkVenue.innerHTML = resData[2].venue;
-
-        // kkrTeamName.innerHTML = resData[3].teamName;
-        // kkrVenue.innerHTML = resData[3].venue;
-        // kkrWinningYears.innerHTML = resData[3].winningYears;
-
-        // miTeamName.innerHTML = resData[4].teamName;
-        // miVenue.innerHTML = resData[4].venue;
-        // miWinningYears.innerHTML = resData[4].winningYears;
-
-        // rrTeamName.innerHTML = resData[5].teamName;
-        // rrVenue.innerHTML = resData[5].venue;
-        // rrWinningYears.innerHTML = resData[5].winningYears;
-
-        // rcbTeamName.innerHTML = resData[6].teamName;
-        // rcbVenue.innerHTML = resData[6].venue;
-
-
-        // srhTeamName.innerHTML = resData[7].teamName;
-        // srhVenue.innerHTML = resData[7].venue;constars;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-
-// for (let i = 0; i < teamData.length; i++) {
-//     console.log(teamsNames[i]);
-//     console.log(teamData[i]);
-//     teamsNames[i].innerHTML = teamData[i].teamName;
-//     teamsVenue[i].innerHTML = teamData[i].venue;
-//     if (teamData[i].winningYears.length != 0) {
-//         teamsWin[i].innerHTML = teamData[i].winningYears;
-//     } else {
-//         teamsWin[i].parentElement.style.visibility = "hidden";
-//     }
-// }
+// The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
+const router = async () => {
+    // Lazy load view element:
+    const content = document.getElementById('page_container');
+    // Get the parsed URl from the addressbar
+    let request = Utils.parseRequestURL()
+    console.log(request);
+    // Parse the URL 
+    let parsedURL = (request.resource ? '/' + request.resource : '/')
+    console.log(parsedURL);
+    // Get the page from our hash of supported routes.
+    // If the parsed URL is not in our list of supported routes, select the 404 page instead
+    let page = routes[parsedURL] ? routes[parsedURL] : Error404
+    content.innerHTML = await page.render();
+    await page.after_render();
+}
+// Listen on hash change:
+window.addEventListener('hashchange', router);
+// Listen on page load:
+window.addEventListener('load', router);
